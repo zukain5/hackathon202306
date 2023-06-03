2623a88bb6a59833a12b236b401dfa1b9720f895
Ziosan13 <yuta.iland.13.nexus@gmail.com>
2023-06-03 13:48:23 +0900

    レクチャー

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,9 @@
 module ActivitiesHelper
+	def format_time_diff(diff)
+		s = diff % 60
+		m = ((diff - s) / 60) % 60
+		h = ((diff - s - m * 60) / (60 ** 2)) % 24
+		d = ((diff - s - m * 60 - h * 60 ** 2) / (60**2 * 24))
+		"#{d.to_i}日 #{h.to_i}時間#{m.to_i}分#{s.to_i}秒"
+	end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,8 @@ class Activity < ApplicationRecord
 	def latest_history_datetime
 		activity_histories.order(acted_at: :desc).first.acted_at
 	end
+	
+	def passed_time
+		Time.current - latest_history_datetime
+	end
 end
