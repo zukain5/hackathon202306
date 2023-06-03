70882d6dd914016b7fa2d77fc1456c2fb6c990da
Kazuki Arai <zukain5@gmail.com>
2023-06-03 14:31:41 +0900

    update format_time_diff

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,9 +1,18 @@
 module ActivitiesHelper
 	def format_time_diff(diff)
-		s = diff % 60
-		m = ((diff - s) / 60) % 60
-		h = ((diff - s - m * 60) / (60 ** 2)) % 24
-		d = ((diff - s - m * 60 - h * 60 ** 2) / (60**2 * 24))
-		"#{d.to_i}日 #{h.to_i}時間#{m.to_i}分#{s.to_i}秒"
+		s = (diff % 60).to_i
+		m = (((diff - s) / 60) % 60).to_i
+		h = (((diff - s - m * 60) / (60 ** 2)) % 24).to_i
+		d = (((diff - s - m * 60 - h * 60 ** 2) / (60**2 * 24))).to_i
+
+    if d >= 1
+      "#{d}日"
+    elsif h >= 1
+      "#{h}時間"
+    elsif m >= 1
+      "#{m}分"
+    else
+      "#{s}秒"
+    end
 	end
 end
